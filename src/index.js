3638125483e5a63ca94e10215ab5368109804052
dianestar <dianestar@naver.com>
2022-06-07 23:32:04 +0900

    2.0~2.4 PURE REDUX: TO DO LIST

--- src/index.js
+++ src/index.js
@@ -1,66 +1,111 @@
 import { legacy_createStore } from "redux";
 
-const plus = document.getElementById("plus");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-// refactoring #2 string 대신 constant variable 사용
-const PLUS = "PLUS";
-const MINUS = "MINUS";
+// 배열을 직접 관리 해줘야 함!
+// const toDos = [];
 
-const countModifier = (count = 0, action) => {
-  console.log(count, action);
+// more comfortable redux way!
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
 
-  // refactoring #1 if-else문을 switch문으로 수정
+// 액션만 넣어주는 함수를 따로 선언해서 사용할 수도 있음
+const addToDo = (text) => {
+  return {
+    type: ADD_TODO,
+    text,
+  };
+}
+
+const deleteToDo = (id) => {
+  return {
+    type: DELETE_TODO,
+    id,
+  };
+}
+
+const reducer = (state = [], action) => {
   switch (action.type) {
-    case PLUS:
-      return count + 1;
-    case MINUS:
-      return count - 1;
+    case ADD_TODO:
+      // state.push(action.text) 불가능!
+      // return [...state, {text: action.text, id: Date.now() }];
+      // 최신순으로 저장하고 싶다면 아래와 같이 수정
+      return [{text: action.text, id: Date.now()}, ...state];
+    case DELETE_TODO:
+      // a filter() method creates a new array with all elements that pass the test!
+      return state.filter((toDo) => toDo.id !== action.id);
     default:
-      return count;
+      return state;
   }
+}
 
-  /*
-  if (action.type === "PLUS") {
-    return count + 1;
-  }
-  else if (action.type === "MINUS") {
-    return count - 1;
-  }
-  else {
-    return count;
-  }
-  */
-};
+const store = legacy_createStore(reducer);
 
-const countStore = legacy_createStore(countModifier);
+store.subscribe(() => console.log(store.getState()) )
 
-// action은 redux에서 function을 부를 때 두 번째 parameter 혹은 argument로
-// reducer와 소통하기 위한 방법
-// actions must be plain objects!
-// actions may not have an undefined "type" property! action은 무조건 type이 있어야함
-// reducer에게 action을 보내는 방법 dispatch
 /*
-countStore.dispatch({type: "PLUS"});
-countStore.dispatch({type: "MINUS"});
+const deleteToDo = (e) => {
+  const id = e.target.parentNode.id;
+  store.dispatch({type: DELETE_TODO, id: id});
+}
 */
 
-const handlePlus = () => {
-  countStore.dispatch({type:PLUS});
+const dispatchDeleteToDo = (e) => {
+  const id = parseInt(e.target.parentNode.id);
+  store.dispatch(deleteToDo(id));
 }
 
-const handleMinus = () =>{
-  countStore.dispatch({type:MINUS});
+const paintToDos = () => {
+  const toDos = store.getState();
+  ul.innerHTML = ""; // 이 코드가 없다면 매번 모든 항목들을 repaint하는 문제 발생
+  toDos.forEach(toDo => {
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.innerText = "DEL";
+    btn.addEventListener("click", dispatchDeleteToDo);
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    li.appendChild(btn);
+    ul.appendChild(li);
+  })
 }
 
-plus.addEventListener("click", handlePlus);
-minus.addEventListener("click", handleMinus);
+store.subscribe(paintToDos);
 
-// subscribe는 store 안에 있는 변화들을 알 수 있게 해줌
-// onChange 함수는 store에 변화가 있을 때마다 감지해서 호출될 것
-const onChange = () => {
-  console.log("there was a change on the store!");
-  number.innerText = countStore.getState();
+const createToDo = (toDo) => {
+  const li = document.createElement("li");
+  li.innerText = toDo;
+  ul.appendChild(li);
 }
-countStore.subscribe(onChange);
+
+/*
+const addToDo = (text) => {
+  store.dispatch({ type: ADD_TODO, text });
+}
+*/
+
+const dispatchAddToDo = (text) => {
+  store.dispatch(addToDo(text));
+}
+
+const onSubmit = (e) => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  // createToDo(toDo);
+
+  // text: toDo 를 추가함으로써 우리가 원하는 텍스트를 액션에 넘겨줄 수 있음
+  // store.dispatch({ type: ADD_TODO, text: toDo });
+  dispatchAddToDo(toDo);
+}
+
+form.addEventListener("submit", onSubmit);
+
+// ⭐ NEVER MUTATE STATE!
+// cf. Three Principles of redux
+// 1. Single source of truth
+// 2. State is read-only (store를 수정할 수 있는 유일한 방법은 action을 보내는 방법 뿐)
+// 3. Changes are made with pure functions (mutating state 하는 대신에 new state objects를 리턴)
+// mutation이란? const friends = [["A"]; friends.push("B"); 변형하는 것!
